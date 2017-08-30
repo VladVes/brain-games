86449edf658d6dab07c68eed30d48ce54b824079
Vladimir Veselov <vccs@inbox.ru>
2017-08-31 00:50:47 +0700

    fix problems linter find

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const greeting = (message) => {
 
 const gamesIntro = () => {
   greeting();
-  const name = getUserName();
+  getUserName();
 };
 
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
@@ -25,22 +25,22 @@ const gameBrainEven = () => {
   greeting(desc);
   const stages = 3;
   const userName = getUserName();
-  const game = (stages) => {
-    if (stages === 0) {
+  const game = (stg) => {
+    if (stg === 0) {
       console.log(`Congratulations, ${userName}`);
       return;
     }
     const num = getRandomInteger(1, 10);
     const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
     console.log(`Question: ${num}`);
-    const answer = readlineSync.question(`Your answer: `);
+    const answer = readlineSync.question('Your answer: ');
     if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correc answer was '${correctAnswer}'\n
         Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
-    return game(stages - 1);
+    game(stg - 1);
   };
   game(stages);
 };
