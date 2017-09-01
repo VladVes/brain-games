f2f5a31020881a885ca13d2ec87eb9b46d728b25
Vladimir Veselov <vccs@inbox.ru>
2017-09-02 03:16:08 +0700

    update getGame

--- src/generic.js
+++ src/generic.js
@@ -1,7 +1,6 @@
 const games = {};
 
-export const getGame = name =>
-  (Object.keys(games).includes(name) ? games[name] : null);
+export const getGame = name => (games[name]);
 
 export const definer = (name, desc, body) => {
   games[name] = { desc, body };
