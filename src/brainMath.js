363f4e6a8bbefc7d1e734cb9bca70cbb62a0126e
Vladimir Veselov <vccs@inbox.ru>
2017-09-02 22:14:08 +0700

    rename findDiv in brainMath

--- src/brainMath.js
+++ src/brainMath.js
@@ -1,9 +1,9 @@
 export const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const findDiv = (n1, n2) => {
+export const findGcd = (n1, n2) => {
   const [dividend, divisor] = n1 >= n2 ? [n1, n2] : [n2, n1];
   if (dividend % divisor === 0) {
     return divisor;
   }
-  return findDiv(dividend % divisor, divisor);
+  return findGcd(dividend % divisor, divisor);
 };
--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,13 +1,11 @@
-import { getRandomInteger, findDiv } from '../brainMath';
+import { getRandomInteger, findGcd } from '../brainMath';
 import gameStarter from '..';
 
 const desc = 'Find the greatest common divisor of given numbers.';
 const game = () => {
   const num1 = getRandomInteger(1, 15);
   const num2 = getRandomInteger(1, 15);
-  const answer = findDiv(num1, num2);
-  const question = `${num1} ${num2}`;
-  return { question, answer: String(answer) };
+  return { question: `${num1} ${num2}`, answer: String(findGcd(num1, num2)) };
 };
 
 export default () => {
